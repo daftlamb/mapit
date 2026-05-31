6586151e8e476436ff766fa0012076452a38430d
Mog <daftlamb@gmail.com>
2026-05-31 20:30:02 +0800

    Revert "Add MapIt Pro desktop licensing"
    This reverts commit d13c4c689d282e10616a9a6e14ea14fbb8b76361.

--- license/admin-preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('licenseAdmin', {
-  generate: (input) => ipcRenderer.invoke('admin:generate', input),
-  copy: (value) => ipcRenderer.invoke('admin:copy', value)
-});
